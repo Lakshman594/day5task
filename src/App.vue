a95763ad2bd5b8eebb2aecbcaeb8174d283c6d49
Lakshman594 <lakshmankumarmattaparthi@gmail.com>
2024-02-14 16:13:10 +0530

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <ChildComp v-for="detail in details" :key="detail.title" :detail="detail" @editData="updateData"/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import ChildComp from './components/ChildComp.vue';
+import {ref} from 'vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
+const details = ref([{title:'lets learn props1',description:"Props are used to change data of child component by parent component1"},
+{title:'lets learn prop2',description:"Props are used to change data of child component by parent component2"},
+{title:'lets learn props3',description:"Props are used to change data of child component by parent component3"}])
+
+const updateData = (DataId)=>{
+  details.value = details.value.map(detail=>{
+    if(detail.title == DataId){
+      return  {...detail, description:'Data is updated'}
+    }
+    return detail
+    
+  })
 }
 </script>
 
---
+++ src/components/ChildComp.vue
@@ -0,0 +1,20 @@
+<template>
+<h1>Hii Good Morning </h1>
+<p >{{detail.title}}</p>
+<p >{{detail.description}}</p>
+<div><button @click.prevent="$emit('editData',detail.title)">Edit Data</button></div>
+<hr />
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+ const props = defineProps({
+    detail:{
+       type: Object,
+       required:true
+    }
+})
+
+console.log(props.detail.title)
+</script>
